cd70387e1b56981b8218f8a6a760a59259737341
Mateusz Borowiak <m.borowiak@hotmail.com>
2019-06-19 14:36:16 +0200

    Added sky shader and its gui controlls

--- js/app.js
+++ js/app.js
@@ -8,6 +8,77 @@ function animate() {
     renderer.render(scene, camera)
 }
 
+// Sky Function
+function initSky() {
+    // Add Sky
+    sky = new THREE.Sky()
+    sky.scale.setScalar(450000)
+    scene.add(sky)
+
+    // Add Sun Helper
+    sunSphere = new THREE.Mesh(
+        new THREE.SphereBufferGeometry(20000, 16, 8),
+        new THREE.MeshBasicMaterial({ color: 0xffffff })
+    )
+    sunSphere.position.y = -700000
+    sunSphere.visible = false
+    scene.add(sunSphere)
+
+    /// GUI
+
+    var effectController = {
+        turbidity: 10,
+        rayleigh: 2,
+        mieCoefficient: 0.005,
+        mieDirectionalG: 0.8,
+        luminance: 1,
+        inclination: 0.49, // elevation / inclination
+        azimuth: 0.25, // Facing front,
+        sun: !true
+    }
+
+    var distance = 400000
+
+    function guiChanged() {
+        var uniforms = sky.material.uniforms
+        uniforms['turbidity'].value = effectController.turbidity
+        uniforms['rayleigh'].value = effectController.rayleigh
+        uniforms['luminance'].value = effectController.luminance
+        uniforms['mieCoefficient'].value = effectController.mieCoefficient
+        uniforms['mieDirectionalG'].value = effectController.mieDirectionalG
+
+        var theta = Math.PI * (effectController.inclination - 0.5)
+        var phi = 2 * Math.PI * (effectController.azimuth - 0.5)
+
+        sunSphere.position.x = distance * Math.cos(phi)
+        sunSphere.position.y = distance * Math.sin(phi) * Math.sin(theta)
+        sunSphere.position.z = distance * Math.sin(phi) * Math.cos(theta)
+
+        sunSphere.visible = effectController.sun
+
+        uniforms['sunPosition'].value.copy(sunSphere.position)
+
+        renderer.render(scene, camera)
+    }
+
+    var gui = new dat.GUI()
+
+    gui.add(effectController, 'turbidity', 1.0, 20.0, 0.1).onChange(guiChanged)
+    gui.add(effectController, 'rayleigh', 0.0, 4, 0.001).onChange(guiChanged)
+    gui.add(effectController, 'mieCoefficient', 0.0, 0.1, 0.001).onChange(
+        guiChanged
+    )
+    gui.add(effectController, 'mieDirectionalG', 0.0, 1, 0.001).onChange(
+        guiChanged
+    )
+    gui.add(effectController, 'luminance', 0.0, 2).onChange(guiChanged)
+    gui.add(effectController, 'inclination', 0, 1, 0.0001).onChange(guiChanged)
+    gui.add(effectController, 'azimuth', 0, 1, 0.0001).onChange(guiChanged)
+    gui.add(effectController, 'sun').onChange(guiChanged)
+
+    guiChanged()
+}
+
 function init() {
     // Scene Setup
     scene = new THREE.Scene()
@@ -49,8 +120,8 @@ function init() {
         1,
         1000
     )
-	camera.position.set(0, 2, 12)
-	scene.add(camera)
+    camera.position.set(0, 2, 12)
+    scene.add(camera)
 
     // Light Setup
     // Ambient Light
@@ -58,9 +129,9 @@ function init() {
     scene.add(ambientLight)
 
     // Point Light
-    const pointLight = new THREE.DirectionalLight(0x1fffff, 1)
-    pointLight.position.set(50, 500, 22)
-    pointLight.target.position.set(300, 400, 200)
+    const pointLight = new THREE.DirectionalLight(0x1fffff, 20)
+    pointLight.position.set(15, 15, 1)
+    pointLight.target.position.set(0, 0, 0)
     console.log(pointLight)
     pointLight.position.set(0, 25, -25)
     pointLight.shadow.camera.near = 0.5
@@ -69,9 +140,7 @@ function init() {
     pointLight.shadow.camera.bottom = -500
     pointLight.shadow.camera.right = 500
     pointLight.shadow.camera.top = 500
-
-    camera.add(pointLight)
-
+    scene.add(pointLight)
 
     // Render Setup
     renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -80,4 +149,7 @@ function init() {
 
     // Camera Controls
     const controls = new THREE.OrbitControls(camera, renderer.domElement)
+
+    // Use Sky
+    initSky()
 }
