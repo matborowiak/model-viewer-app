158a0bea13c7c8b8ace28062662e618735fa844a
Mateusz Borowiak <m.borowiak@hotmail.com>
2019-06-19 13:32:37 +0200

    Fixed and cleaned big chunk of code + added new 3d model

--- js/app.js
+++ js/app.js
@@ -1,73 +1,64 @@
-var camera, scene, renderer;
-var geometry, material, mesh, mesh2;
+let camera, scene, renderer
+let geometry, material, mesh, mesh2
 
-init();
-animate();
-
-function init() {
-
-	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
-	scene = new THREE.Scene();
-	geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-	material = new THREE.MeshNormalMaterial();
-	mesh = new THREE.Mesh(geometry, material);
-
-	scene.add(mesh);
-	
-	// Load custom model
-	var loader = new THREE.GLTFLoader();
-	loader.load('house/scene.gltf', function (gltf) {
-		gltf.scene.position.x = 0;
-		scene.add(gltf.scene);
-
-		gltf.animations; // Array<THREE.AnimationClip>
-		gltf.scene; // THREE.Scene
-		gltf.scenes; // Array<THREE.Scene>
-		gltf.cameras; // Array<THREE.Camera>
-		gltf.asset; // Object
-		debugger
-		// return mesh2 = gltf.scene
-	}, undefined, function (error) {
-		console.error(error);
-	});
-
-	
-	var light = new THREE.AmbientLight(0x404040, 10); // soft white light
-	scene.add(light);
-
-	renderer = new THREE.WebGLRenderer({ antialias: true });
-	renderer.setSize(window.innerWidth, window.innerHeight);
-
-	// Camera Controlls
-	var controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-	//controls.update() must be called after any manual changes to the camera's transform
-	camera.position.set(0, 0, 2);
-	controls.update();
-
-	function animate() {
-
-		requestAnimationFrame(animate);
-
-		// required if controls.enableDamping or controls.autoRotate are set to true
-		controls.update();
-
-		renderer.render(scene, camera);
-
-	}
-
-	document.body.appendChild(renderer.domElement);
-
-}
+init()
+animate()
 
 function animate() {
+	requestAnimationFrame(animate)
+	renderer.render(scene, camera)
+}
 
-	requestAnimationFrame(animate);
+function init() {
+	// Load custom GLTF model
+	const loader = new THREE.GLTFLoader()
+	loader.load(
+		'/model3/scene.gltf',
+		function (gltf) {
+			const model = gltf.scene.children[0]
+			model.scale.set(0.01, 0.01, 0.01)
+			model.rotation.set(1.5707963267948963, 0, -0.8)
+			model.receiveShadow = true
+			console.log(model)
+			scene.add(model)
 
-	// mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.02;
-	// mesh2.rotation.y += 0.02;
+		},
+		undefined,
+		function (error) {
+			console.error(error)
+		}
+	)
 
-	renderer.render(scene, camera);
+	// Scene Setup
+	scene = new THREE.Scene()
+	scene.background = new THREE.Color(0xddddddd);
 
+	// Camera Setup
+	camera = new THREE.PerspectiveCamera(
+		70,
+		window.innerWidth / window.innerHeight,
+		1,
+		1000
+	)
+	camera.position.set(0, 2, 12)
+
+	// Light Setup
+	// Ambient Light
+	const ambientLight = new THREE.AmbientLight(0xcccccc, 1)
+	scene.add(ambientLight)
+
+	// Point Light
+	const pointLight = new THREE.PointLight(0x1fffff, 1)
+	console.log(pointLight)
+	pointLight.position.set(0, 25, -25)
+	camera.add(pointLight)
+	scene.add(camera)
+
+	// Render Setup
+	renderer = new THREE.WebGLRenderer({ antialias: true })
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	document.body.appendChild(renderer.domElement)
+
+	// Camera Controls
+	const controls = new THREE.OrbitControls(camera, renderer.domElement)
 }
